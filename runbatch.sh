d3bfeec2d28351435ea6d4905a7665c565b9c75d
adrianskw <adrianskw@gmail.com>
2017-10-27 16:13:50 -0700

    Added a baisc bash script

---
+++ runbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Quick scripting for doing a batch of annealLorenz
+# You can just set the values of interest and go do something else
+# Remember, annealLorenz takes in 4 arguments:
+# D = number of variables
+# M = number of timesteps
+# dt = size of timestep
+# L_frac = fraction of D that is "measured"
+# run the code as:
+#   bash runbatch.sh
+python annealLorenz.py 5 100 0.2 0.4 >> logs/out.log
+echo "DONE 1"
+python annealLorenz.py 5 40 0.5 0.4 >> logs/out.log
+echo "DONE 2"
